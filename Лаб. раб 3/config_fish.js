1305f7a115b43d38b63c26a82d569b7873068413
web0Impulse <smoug2001@mail.ru>
2025-11-18 01:06:41 +0400

    Add config template

---
+++ Лаб. раб 3/config_fish.js
@@ -0,0 +1,12 @@
+// Конфигурационный файл
+
+// Настройки подключения к БД
+const DB_CONFIG = {
+  host: "localhost",
+  user: "root",
+  database: "db_name",
+  password: "root",
+  dateStrings: true,
+}
+
+export { DB_CONFIG }
