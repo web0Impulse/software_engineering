b0b1dd0e8bd13b03b6664477e7bd45a8eca447f1
web0Impulse <smoug2001@mail.ru>
2025-11-18 00:02:06 +0400

    Add company controller

---
+++ Лаб. раб 3/controllers/company_controller.js
@@ -0,0 +1,55 @@
+// контроллер Пользователя
+// TODO: Добавить валидацию
+import { Company } from "../models/company_model.js";
+import { databaseErrorHandler } from "../handlers/error_handler.js";
+import crypto from "crypto";
+
+export class CompanyController {
+  // Метод создания пользователя
+  static async create (request, response) {
+    // Считывание данных
+    let companyData = request.body;
+    // Создание хэша пароля
+    let passwordHash = crypto.createHash("sha256")
+      .update(companyData.password)
+      .digest("hex");
+    // Внесение нового пользователя в БД
+    // TODO: вернуть токен или сессию
+    const company = structuredClone(Company);
+    company.create({
+        name: companyData.company_name,
+        login: companyData.login,
+        password_hash: passwordHash,
+    }).then((result) => {
+        response.status(201).json({
+            status: 201,
+            data: result,
+        })
+    }).catch(err => {
+        databaseErrorHandler(err, response);
+    });
+  }
+
+  // Метод входа
+  static async login(request, response) {
+    // Извлекаем данные
+    const data = request.body;
+    // Хэшируем пароль
+    const passwordHash = crypto.createHash("sha256")
+      .update(data.password)
+      .digest("hex");
+    // Ищем компанию
+    // TODO: вернуть токен или сессию
+    const company = structuredClone(Company);
+    company.getAll("WHERE login = ? AND password = ?", [data.login, passwordHash])
+        .then((result) => {
+            return response.status(200).json({
+                status: 200,
+                data: result[0]
+            })
+        })
+        .catch(err => {
+            databaseErrorHandler(err, response);
+        });
+  }
+}
