6c9597deac246e665ecab416f7609abb1fcf8cdb
web0Impulse <smoug2001@mail.ru>
2025-11-18 14:36:58 +0400

    Add api/ship routes with validation

--- Лаб. раб 3/app.js
+++ Лаб. раб 3/app.js
@@ -1,10 +1,11 @@
-import express from "express"; // подключение express
-import { body, validationResult } from "express-validator"; // Подключение express-validator
+import express, { application } from "express"; // подключение express
+import { body, param } from "express-validator"; // Подключение express-validator
 import { MySQL_CONNECTION } from "./sql_connection.js"; // импорт подключения к бд
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import { CompanyController } from "./controllers/company_controller.js";
 import { PropertyController } from "./controllers/property_controller.js";
+import { ShipController } from "./controllers/ship_controller.js";
 
 // TODO: Добавить валидацию
 
@@ -36,6 +37,32 @@ app.get("/signup", function(request, response){
     response.sendFile(__dirname + "/pages/signup.html");
 });
 app.post("/signup", jsonParser, CompanyController.create);
+
+// api ship route
+app.get("/api/ship/:id", [
+  param('id').isInt().withMessage("ID должен быть числом"),
+], ShipController.get);
+app.post("/api/ship", jsonParser, [
+  body("name")
+    .notEmpty().withMessage("Имя корабля обязательно")
+    .isLength({ max: 50 }).withMessage("Имя корабля должно быть не более 50 символов"),
+  body("company_id")
+    .isInt().withMessage("Company_ID должен быть числом")
+], ShipController.create);
+app.post("/api/ship/:id", jsonParser, [
+  param("id").isInt().withMessage("ID должен быть числом"),
+  body("name")
+    .isLength({ max: 50 }).withMessage("name должно быть не более 50 символов"),
+  body("company_id")
+    .isInt().withMessage("company_id должен быть числом"),
+  body("created_at")
+    .isDate().withMessage("created_at должно быть датой"),
+  body("updated_at")
+    .isDate().withMessage("updated_at должно быть датой")  
+], ShipController.update);
+app.delete("/api/ship/:id", [
+  param("id").isInt().withMessage("ID должен быть числом"),
+], ShipController.delete);
 
 // ответ по умолчанию
 app.get("/*", function(request, response){
