48a6d6bddae4916a2f24ac47dc61e1f3a405e314
web0Impulse <smoug2001@mail.ru>
2025-11-18 15:46:00 +0400

    Add propertyTypeModel

---
+++ Лаб. раб 3/models/property_type_model.js
@@ -0,0 +1,35 @@
+import { ModelBase } from "../models/model_base.js";
+
+
+/**
+ * Модель компании
+ */
+export class PropertyType extends ModelBase {
+  constructor() {
+    super({
+      table: "property_type",
+      fields: [
+        "id",
+        "name",
+        "parent_id",
+        "created_at",
+        "updated_at",
+      ],
+      aliases: [],
+      fillable: [
+        "name",
+        "parent_id",
+        "created_at",
+        "updated_at",
+      ],
+      visible: [
+        "id",
+        "name",
+        "parent_id",
+        "created_at",
+        "updated_at",
+      ],
+      timestamp: true,
+    });
+  }
+}
