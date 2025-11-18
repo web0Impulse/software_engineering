1cdee3287f9a72f7eacf8243f4e1898f4f47ef9b
web0Impulse <smoug2001@mail.ru>
2025-11-18 14:37:27 +0400

    Add ship controller

---
+++ Лаб. раб 3/controllers/ship_controller.js
@@ -0,0 +1,157 @@
+// контроллер Кораблей
+// TODO: Добавить валидацию
+import { Ship } from "../models/ship_model.js";
+import { databaseErrorHandler, DB_ERR_CODES } from "../handlers/error_handler.js";
+import { validationResult } from "express-validator";
+import { validationErrorHandler } from "../handlers/validation_errors_handler.js";
+
+export class ShipController {
+    // Метод возврата по ид
+    static async get (request, response) {
+        // Проверка ошибок валидации
+        const errors = validationResult(request);
+        if (!errors.isEmpty()) {
+            return validationErrorHandler({
+                status: 400,
+                message: "Ошибка в запросе",
+                errors: errors.array(),
+            }, response);
+        }
+        const shipId = request.params["id"];
+        const ship = new Ship();
+        ship.getAll("WHERE id = ?", [shipId])
+            .then((result) => {
+                return response
+                    .status(200)
+                    .json({
+                        status: 200,
+                        data: result[0] || {}
+                    });
+            })
+            .catch(err => {
+                databaseErrorHandler(err, response);
+            })
+    }
+
+
+  // Метод создания корабля
+  static async create (request, response) {
+    // Проверка ошибок валидации
+        const errors = validationResult(request);
+        if (!errors.isEmpty()) {
+            return validationErrorHandler({
+                status: 400,
+                message: "Ошибка в запросе",
+                errors: errors.array(),
+            }, response);
+        }
+
+    // Считывание данных
+    const shipData = request.body;
+    // Внесение нового корябля в БД
+    // TODO: вернуть токен или сессию
+    // TODO: добавить проверку по company_id
+    const ship = new Ship();
+    ship.create({
+        name: shipData.name,
+        company_id: shipData.company_id,
+    }).then((result) => {
+        response.status(201).json({
+            status: 201,
+            data: result,
+        })
+    }).catch(err => {
+        databaseErrorHandler(err, response);
+    });
+  }
+
+  // Метод обновления данных корабля
+  static async update (request, response) {
+    // Проверка ошибок валидации
+        const errors = validationResult(request);
+        if (!errors.isEmpty()) {
+            return validationErrorHandler({
+                status: 400,
+                message: "Ошибка в запросе",
+                errors: errors.array(),
+            }, response);
+        }
+    // Считывание данных
+    const requestData = request.body;
+    const updShipObj = {};
+    if (requestData.name) {
+        updShipObj.name = requestData.name;
+    }
+    if (requestData.company_id) {
+        updShipObj.company_id = requestData.company_id;
+    }
+    if (requestData.created_at) {
+        updShipObj.created_at = requestData.created_at;
+    }
+    if (requestData.updated_at) {
+        updShipObj.updated_at = requestData.updated_at;
+    }
+    // Внесение нового корябля в БД
+    // TODO: вернуть токен или сессию
+    // TODO: добавить проверку по company_id
+    const ship = new Ship();
+    ship.getAll("WHERE id = ?", request.params["id"])
+        .then(() => {
+            if (!ship.values.length) {
+                throw {
+                    code: DB_ERR_CODES.ER_NO_REFERENCED_ROW,
+                    message: "Корабля с таким ID не найдено",
+                }
+            }
+            ship.update(updShipObj)
+                .then(() => {
+                    ship.getAll("WHERE id = ?", request.params["id"])
+                        .then((result) => {
+                            return response
+                                .status(200)
+                                .json({
+                                    status:200,
+                                    data: result[0]
+                                })
+                        })
+                })
+        })
+        .catch((err) => {
+            databaseErrorHandler(err, response);
+        });
+  }
+
+  // Удалить корабль из БД
+  static async delete(request, response) {
+    // Проверка ошибок валидации
+        const errors = validationResult(request);
+        if (!errors.isEmpty()) {
+            return validationErrorHandler({
+                status: 400,
+                message: "Ошибка в запросе",
+                errors: errors.array(),
+            }, response);
+        }
+    const ship = new Ship();
+    ship.getAll("WHERE id = ?", request.params["id"])
+        .then(() => {
+            if (!ship.values.length) {
+                throw {
+                    code: DB_ERR_CODES.ER_NO_REFERENCED_ROW,
+                    message: "Корабля с таким ID не найдено",
+                }
+            }
+            ship.deleteValues()
+                .then(() => {
+                    return response
+                        .status(204)
+                        .json({
+                            status: 204
+                        })
+                })
+        })
+        .catch(err => {
+            databaseErrorHandler(err, response);
+        });
+  }
+}
