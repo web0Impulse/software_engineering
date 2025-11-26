a579ca8e69c74cecde8ecadcb20ffe55d57f0887
web0Impulse <smoug2001@mail.ru>
2025-11-26 22:21:59 +0400

    Add api property route

--- Лаб. раб 3/app.js
+++ Лаб. раб 3/app.js
@@ -25,7 +25,7 @@ app.get("/", function(request, response){
 });
 
 // test
-app.get("/test", PropertyController.test);
+//app.get("/test", PropertyController.test);
 
 // определяем обработчик для маршрута входа
 app.get("/login", function(request, response){
@@ -61,14 +61,72 @@ app.post("/api/ship/:id", jsonParser, [
     .isInt().withMessage("company_id должен быть числом"),
   body("created_at")
     .optional()
-    .isDate().withMessage("created_at должно быть датой"),
+    .isISO8601().withMessage("created_at должно быть датой"),
   body("updated_at")
     .optional()
-    .isDate().withMessage("updated_at должно быть датой")  
+    .isISO8601().withMessage("updated_at должно быть датой")  
 ], ShipController.update);
 app.delete("/api/ship/:id", [
   param("id").isInt().withMessage("ID должен быть числом"),
 ], ShipController.delete);
+
+// api property route
+app.get("/api/property/:id", [
+  param("id").isInt().withMessage("ID должен быть числом")
+], PropertyController.get);
+app.post("/api/property", jsonParser, [
+  body("name")
+    .isString().withMessage("name должно быть строкой")
+    .notEmpty().withMessage("name обязательно")
+    .isLength({ max: 50 }).withMessage("name должно быть не более 50 символов"),
+  body("type_id")
+    .isInt().withMessage("type_id должно быть числом"),
+  body("quantity")
+    .isInt().withMessage("quantity должно быть числом"),
+  body("location")
+    .optional()
+    .isLength({ max: 50 }).withMessage("location должно быть не более 50 символов"),
+  body("date_prev_inspection")
+    .isISO8601().withMessage("date_prev_inspection должно быть датой"),
+  body("frequency_of_inspection")
+    .isInt({ min: 0, max: 159000000000 }).withMessage("frequency_of_inspection должно быть числом"),
+  body("check_mark")
+    .isBoolean().withMessage("check_mark должно быть булевым значением"),
+  body("is_ok")
+    .isBoolean().withMessage("is_ok должно быть булевым значением")
+], PropertyController.create);
+app.post("/api/property/:id", jsonParser, [
+  param("id").isInt().withMessage("ID должен быть числом"),
+  body("name")
+    .optional()
+    .isString().withMessage("name должно быть строкой")
+    .notEmpty().withMessage("name обязательно")
+    .isLength({ max: 50 }).withMessage("name должно быть не более 50 символов"),
+  body("type_id")
+    .optional()
+    .isInt().withMessage("type_id должно быть числом"),
+  body("quantity")
+    .optional()
+    .isInt().withMessage("quantity должно быть числом"),
+  body("location")
+    .optional()
+    .isLength({ max: 50 }).withMessage("location должно быть не более 50 символов"),
+  body("date_prev_inspection")
+    .optional()
+    .isISO8601().withMessage("date_prev_inspection должно быть датой"),
+  body("frequency_of_inspection")
+    .optional()
+    .isInt({ min: 0, max: 159000000000 }).withMessage("frequency_of_inspection должно быть числом"),
+  body("check_mark")
+    .optional()
+    .isBoolean().withMessage("check_mark должно быть булевым значением"),
+  body("is_ok")
+    .optional()
+    .isBoolean().withMessage("is_ok должно быть булевым значением")
+], PropertyController.update);
+app.delete("/api/property/:id", [
+  param("id").isInt().withMessage("ID должен быть числом"),
+], PropertyController.delete);
 
 // api property type route
 app.get("/api/propertyType/:id", [
@@ -92,10 +150,10 @@ app.post("/api/propertyType/:id", jsonParser, [
     .isInt().withMessage("parent_id должен быть числом"),
   body("created_at")
     .optional()
-    .isDate().withMessage("created_at должно быть датой"),
+    .isISO8601().withMessage("created_at должно быть датой"),
   body("updated_at")
     .optional()
-    .isDate().withMessage("updated_at должно быть датой")  
+    .isISO8601().withMessage("updated_at должно быть датой")  
 ], PropertyTypeController.update);
 app.delete("/api/propertyType/:id", [
   param("id").isInt().withMessage("ID должен быть числом"),
