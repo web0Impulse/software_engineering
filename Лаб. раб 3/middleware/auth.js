39d7d66a64a57b24e9d2ed0440685b0a0a1b057d
web0Impulse <smoug2001@mail.ru>
2025-12-23 21:18:58 +0400

    Добавлена проверка для динамической переадресации

--- Лаб. раб 3/middleware/auth.js
+++ Лаб. раб 3/middleware/auth.js
@@ -1,6 +1,8 @@
 export function checkAuth(req, res, next) {
   const isAuth = req.session && req.session.user;
-  const publicPaths = ['/login', '/api/login', '/signup', '/api/signup']; // публичные страницы без авторизации
+  // const publicPaths = ['/login', '/api/login', '/signup', '/api/signup']; // публичные страницы без авторизации
+  const publicPaths = ['/login', '/signup'];
+  const publicApiPaths = ['/api/login', '/api/signup'];
 
   // Если пользователь авторизован и заходит на страницу входа или регистрации — перенаправляем на главную
   if (isAuth && publicPaths.includes(req.path)) {
@@ -8,8 +10,16 @@ export function checkAuth(req, res, next) {
   }
 
   // Если пользователь не авторизован и заходит не на публичные страницы — перенаправляем на /login
-  if (!isAuth && !publicPaths.includes(req.path)) {
-    return res.redirect('/login');
+  if (!isAuth) {
+    if (req.path == '/') {
+      return res.redirect('/login');
+    } else if (!publicPaths.includes(req.path) && !publicApiPaths.includes(req.path)) {
+      // Иначе
+      // TODO добавить обработку чтобы пропускало на несуществующие страницы
+      return res.status(403).json({
+        data: "Unauthorized"
+      });
+    }    
   }
 
   // Во всех остальных случаях — разрешаем доступ
