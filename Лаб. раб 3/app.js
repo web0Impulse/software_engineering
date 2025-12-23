396ea99c845b3d99ab7bd178e2c4a2c0a71388bf
web0Impulse <smoug2001@mail.ru>
2025-12-23 21:17:20 +0400

    Over Multiple changes

--- Лаб. раб 3/app.js
+++ Лаб. раб 3/app.js
@@ -9,8 +9,8 @@ import { PropertyController } from "./controllers/property_controller.js";
 import { ShipController } from "./controllers/ship_controller.js";
 import { PropertyTypeController } from "./controllers/property_type_controller.js";
 import { checkAuth } from "./middleware/auth.js";
-
-// TODO: Добавить валидацию
+import hbs from 'hbs';
+import { Property } from "./models/property_model.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -31,62 +31,98 @@ app.use(checkAuth);
 
 
 // определяем обработчик для главной страницы
-app.get("/", function(request, response){
-    response.sendFile(__dirname + "/pages/main.html");
+app.get("/", function(_, response){
+    // response.sendFile(__dirname + "/pages/main.html");
+    const property = new Property();
+    property.getAll()
+      .then((results) => {
+          response.render("index.hbs", {property: property});
+      });
+    
 });
 
 // test
 //app.get("/test", PropertyController.test);
 
-// определяем обработчик для маршрута входа
-app.get("/login", function(request, response){
+// 1. Маршруты для авторизации
+// правила валидации авторизации
+const loginValidationRules = [
+  body("login")
+    .notEmpty().withMessage("Логин не должен быть пустым")
+    .isString().withMessage("Логин должен быть строкой")
+    .isLength({ max: 50 }).withMessage("Длина логина не должна превышать 50 символов"),
+  body("password")
+    .notEmpty().withMessage("Пароль не должен быть пустым")
+    .isString().withMessage("Пароль должен быть строкой")
+];
+// Страница авторизации
+app.get("/login", function(_, response){
     response.sendFile(__dirname + "/pages/login.html");
 });
-app.post("/api/login", jsonParser, CompanyController.login);
+// Маршрут логики авторизции
+app.post("/api/login", jsonParser, loginValidationRules, CompanyController.login);
 
-// определяем обработчик для маршрута регистрации
-app.get("/signup", function(request, response){
+// 2. Маршруты для регистрации
+// Правила валидации регистрации
+let signupValidationRules = [
+  body("company_name")
+    .notEmpty().withMessage("Название компании не должно быть пустым")
+    .isString().withMessage("Название компании должно быть строкой")
+    .isLength({ max: 50 }).withMessage("Длина названия компании не должна превышать 50 символов"),
+  body("login")
+    .notEmpty().withMessage("Логин не должен быть пустым")
+    .isString().withMessage("Логин должен быть строкой")
+    .isLength({ max: 50 }).withMessage("Длина логина не должна превышать 50 символов"),
+  body("password")
+    .notEmpty().withMessage("Пароль не должен быть пустым")
+    .isString().withMessage("Пароль должен быть строкой")
+];
+// Страница регистрации
+app.get("/signup", function(_, response){
     response.sendFile(__dirname + "/pages/signup.html");
 });
-app.post("/signup", jsonParser, CompanyController.create);
+// Маршрут для логики регистрации
+app.post("/api/signup", jsonParser, signupValidationRules, CompanyController.create);
 
+// 3. Маршрут выхода
 app.post("/api/logout", CompanyController.logout);
 
-// api ship route
+// 4. Маршруты для кораблей
+// Маршрут получения данных о корабле по ID
 app.get("/api/ship/:id", [
   param('id').isInt().withMessage("ID должен быть числом"),
 ], ShipController.get);
+// Маршрут создания корабля
 app.post("/api/ship", jsonParser, [
   body("name")
     .notEmpty().withMessage("name обязательно")
-    .isLength({ max: 50 }).withMessage("name должно быть не более 50 символов"),
-  body("company_id")
-    .notEmpty().withMessage("company_id должно быть числом")
-    .isInt().withMessage("Company_ID должен быть числом")
+    .isString().withMessage("name должно быть строкой")
+    .isLength({ max: 50 }).withMessage("name должно быть не более 50 символов")
 ], ShipController.create);
+// Маршрут изменения данных корабля
 app.post("/api/ship/:id", jsonParser, [
   param("id").isInt().withMessage("ID должен быть числом"),
   body("name")
     .optional()
+    .isString().withMessage("name должно быть строкой")
     .isLength({ max: 50 }).withMessage("name должно быть не более 50 символов"),
-  body("company_id")
-    .optional()
-    .isInt().withMessage("company_id должен быть числом"),
-  body("created_at")
-    .optional()
-    .isISO8601().withMessage("created_at должно быть датой"),
-  body("updated_at")
-    .optional()
-    .isISO8601().withMessage("updated_at должно быть датой")  
+  // body("created_at")
+  //   .optional()
+  //   .isISO8601().withMessage("created_at должно быть датой"),
+  // body("updated_at")
+  //   .optional()
+  //   .isISO8601().withMessage("updated_at должно быть датой")  
 ], ShipController.update);
 app.delete("/api/ship/:id", [
   param("id").isInt().withMessage("ID должен быть числом"),
 ], ShipController.delete);
 
-// api property route
+// 5. Маршруты для собственности
+// Маршрут для получения собственности по ID
 app.get("/api/property/:id", [
   param("id").isInt().withMessage("ID должен быть числом")
 ], PropertyController.get);
+// Маршрут для создания собственности
 app.post("/api/property", jsonParser, [
   body("name")
     .isString().withMessage("name должно быть строкой")
@@ -94,6 +130,8 @@ app.post("/api/property", jsonParser, [
     .isLength({ max: 50 }).withMessage("name должно быть не более 50 символов"),
   body("type_id")
     .isInt().withMessage("type_id должно быть числом"),
+  body("ship_id")
+    .isInt().withMessage("ship_id Должен быть числом"),
   body("quantity")
     .isInt().withMessage("quantity должно быть числом"),
   body("location")
@@ -108,6 +146,7 @@ app.post("/api/property", jsonParser, [
   body("is_ok")
     .isBoolean().withMessage("is_ok должно быть булевым значением")
 ], PropertyController.create);
+// Маршрут для изменения собственности по ID
 app.post("/api/property/:id", jsonParser, [
   param("id").isInt().withMessage("ID должен быть числом"),
   body("name")
@@ -118,6 +157,9 @@ app.post("/api/property/:id", jsonParser, [
   body("type_id")
     .optional()
     .isInt().withMessage("type_id должно быть числом"),
+  body("ship_id")
+    .optional()
+    .isInt().withMessage("ship_id должен быть числом"),
   body("quantity")
     .optional()
     .isInt().withMessage("quantity должно быть числом"),
@@ -137,37 +179,44 @@ app.post("/api/property/:id", jsonParser, [
     .optional()
     .isBoolean().withMessage("is_ok должно быть булевым значением")
 ], PropertyController.update);
+// Маршрут для удаления собственности по ID
 app.delete("/api/property/:id", [
   param("id").isInt().withMessage("ID должен быть числом"),
 ], PropertyController.delete);
 
-// api property type route
+// 6. Маршруты для типов собственности
+// Маршрут получения типа по ID
 app.get("/api/propertyType/:id", [
   param('id').isInt().withMessage("ID должен быть числом"),
 ], PropertyTypeController.get);
+// Маршрут создания типа
 app.post("/api/propertyType", jsonParser, [
   body("name")
     .notEmpty().withMessage("name обязательно")
+    .isString().withMessage("name должно быть строкой")
     .isLength({ max: 50 }).withMessage("name должно быть не более 50 символов"),
   body("parent_id")
     .optional()
     .isInt().withMessage("parent_id должен быть числом")
 ], PropertyTypeController.create);
+// Маршрут обновления данных типа по ID
 app.post("/api/propertyType/:id", jsonParser, [
   param("id").isInt().withMessage("ID должен быть числом"),
   body("name")
     .optional()
+    .isString().withMessage("name должно быть строкой")
     .isLength({ max: 50 }).withMessage("name должно быть не более 50 символов"),
   body("parent_id")
-    .optional()
+    .optional({ nullable: true })
     .isInt().withMessage("parent_id должен быть числом"),
-  body("created_at")
-    .optional()
-    .isISO8601().withMessage("created_at должно быть датой"),
-  body("updated_at")
-    .optional()
-    .isISO8601().withMessage("updated_at должно быть датой")  
+  // body("created_at")
+  //   .optional()
+  //   .isISO8601().withMessage("created_at должно быть датой"),
+  // body("updated_at")
+  //   .optional()
+  //   .isISO8601().withMessage("updated_at должно быть датой")  
 ], PropertyTypeController.update);
+// Маршрут удаления типа
 app.delete("/api/propertyType/:id", [
   param("id").isInt().withMessage("ID должен быть числом"),
 ], PropertyTypeController.delete);
