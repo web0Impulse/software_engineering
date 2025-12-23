edf0c064ee2b109936239b2ea567a61e18dd8c7b
web0Impulse <smoug2001@mail.ru>
2025-12-23 20:17:47 +0400

    Добавлена проверка на принадлежность компании в методе get

--- Лаб. раб 3/controllers/property_controller.js
+++ Лаб. раб 3/controllers/property_controller.js
@@ -1,32 +1,60 @@
 // контроллер Имущества
 // TODO: Добавить валидацию
 import { Property } from "../models/property_model.js";
-import { databaseErrorHandler } from "../handlers/error_handler.js";
+import { databaseErrorHandler, DB_ERR_CODES } from "../handlers/error_handler.js";
 import { validationResult } from "express-validator";
 import { validationErrorHandler } from "../handlers/validation_errors_handler.js";
+import { PropertyType } from "../models/property_type_model.js";
+import { Ship } from "../models/ship_model.js";
 
 export class PropertyController {
-  static async get(request, response) {
-      // Проверка ошибок валидации
-      const errors = validationResult(request);
-      if (!errors.isEmpty()) {
-          return validationErrorHandler({
-              status: 400,
-              message: "Ошибка в запросе",
-              errors: errors.array(),
-          }, response);
-      }
-      const propertyId = request.params["id"];
-      const property = new Property();
-      property.getAll("WHERE id = ?", [propertyId])
-          .then((result) => {
-              return response
-                  .status(200)
-                  .json({
-                      status: 200,
-                      data: result[0] || {}
-                  });
-              })
+    static async get(request, response) {
+        // Проверка ошибок валидации
+        const errors = validationResult(request);
+        if (!errors.isEmpty()) {
+            return validationErrorHandler({
+                status: 400,
+                message: "Ошибка в запросе",
+                errors: errors.array(),
+            }, response);  
+        }
+        // Извлечь ID
+        const propertyId = request.params["id"];
+        const property = new Property();
+        // Найти Property по ID
+        property.getAll("WHERE id = ?", [propertyId])
+            .then((result) => {
+                if (result[0]) {
+                    // Проверка на принадлежность Property -> Ship -> Company
+                    const ship = new Ship();
+                    return ship.getAll("WHERE id = ? AND company_id = ?", [result[0].ship_id, request.session.user.id])
+                        .then((result) => {
+                            if (result[0]) {
+                                return response
+                                    .status(200)
+                                    .json({
+                                        status: 200,
+                                        data: property.values[0]
+                                    });
+                            } else {
+                                return response
+                                    .status(200)
+                                    .json({
+                                        status: 200,
+                                        data: {}
+                                    });
+                            }
+                        })
+                } else {
+                    return response
+                        .status(200)
+                        .json({
+                            status: 200,
+                            data: {}
+                        });
+                }
+          })
+          // Отлов ошибок
           .catch(err => {
               databaseErrorHandler(err, response);
           })
@@ -46,30 +74,57 @@ export class PropertyController {
       // Считывание данных
       const propertyData = request.body;
       // Внесение propertyType в БД
-      // TODO: добавить проверку по type_id
-      const property = new Property();
-      // расчет dateNextInspection
-      const datePrevInspectionParsed = new Date(propertyData.date_prev_inspection);
-      const dateNextInspection = new Date(datePrevInspectionParsed.getTime() + propertyData.frequency_of_inspection);
-      
-      property.create({
-          name: propertyData.name,
-          type_id: propertyData.type_id,
-          quantity: propertyData.quantity,
-          location: propertyData.location || "",
-          date_prev_inspection: propertyData.date_prev_inspection,
-          frequency_of_inspection: propertyData.frequency_of_inspection,
-          date_next_inspection: dateNextInspection,
-          check_mark: propertyData.check_mark,
-          is_ok: propertyData.is_ok,
-      }).then((result) => {
-          response.status(201).json({
-              status: 201,
-              data: result,
-          })
-      }).catch(err => {
-          databaseErrorHandler(err, response);
-      });
+      // Проверка по type_id
+      const propType = new PropertyType();
+      propType.getAll("WHERE id = ?", [propertyData.type_id])
+            .then((result) => {
+                if (!result[0]) {
+                    throw {
+                        code: DB_ERR_CODES.ER_NO_REFERENCED_ROW,
+                        message: "Типа имущества с таким ID не найдено",
+                    }
+                }
+            })
+            .then(() => {
+                // провекра по ship_id
+                const ship = new Ship();
+                return ship.getAll("WHERE id = ? AND company_id = ?", [propertyData.ship_id, request.session.user.id])
+                    .then((result) => {
+                        if (!result[0]) {
+                            throw {
+                                code: DB_ERR_CODES.ER_NO_REFERENCED_ROW,
+                                message: "Корабля с таким ID не найдено",
+                            }
+                        }
+                    })
+                    .then(() => {
+                        const property = new Property();
+                        // расчет dateNextInspection
+                        const datePrevInspectionParsed = new Date(propertyData.date_prev_inspection);
+                        const dateNextInspection = new Date(datePrevInspectionParsed.getTime() + propertyData.frequency_of_inspection);
+
+                        return property.create({
+                            name: propertyData.name,
+                            type_id: propertyData.type_id,
+                            quantity: propertyData.quantity,
+                            ship_id: propertyData.ship_id,
+                            location: propertyData.location || "",
+                            date_prev_inspection: propertyData.date_prev_inspection,
+                            frequency_of_inspection: propertyData.frequency_of_inspection,
+                            date_next_inspection: dateNextInspection,
+                            check_mark: propertyData.check_mark,
+                            is_ok: propertyData.is_ok,
+                        }).then((result) => {
+                            response.status(201).json({
+                                status: 201,
+                                data: result,
+                            })
+                        })
+                    })
+            })
+            .catch(err => {
+                databaseErrorHandler(err, response);
+            });
     }
   
     static async update(request, response) {
